05633650027e5c2233d5921dffaf22b67dd64f8c
monicaleep <monica.parrillo@gmail.com>
2020-09-13 11:44:44 -0500

    finish with bruno

--- scripts/materialize.js
+++ scripts/materialize.js
@@ -1,1 +1,22 @@
-console.log("Ciao Bella, Mi piace parlare italiano");
+// document.addEventListener('DOMContentLoaded', function() {
+//   var elems = document.querySelectorAll('.sidenav');
+//   var instances = M.Sidenav.init(elems, options);
+// });
+
+// mobile hamburger menu
+const mobileMenu = document.querySelectorAll('.sidenav')
+M.Sidenav.init(mobileMenu,{})
+
+
+// slider Sections
+const element = document.querySelectorAll('.slider');
+M.Slider.init(element,{
+  indicators: false,
+  height: 530,
+  transition: 550,
+  interval: 6000
+})
+
+// image material boxed
+const image = document.querySelectorAll('.materialboxed');
+M.Materialbox.init(image, {});
